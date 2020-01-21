4bcbd8b8dce2e9abac492d9d0454a4f5d7ecfdf3
Fiona <fyeung21@gmail.com>
2020-01-20 19:27:59 -0800

    edit navigation bar with correct styles

--- js/navigation/NavigationLayout.js
+++ js/navigation/NavigationLayout.js
@@ -1,5 +1,6 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
 import AboutScreen from '../screens/About';
 import MapScreen from '../screens/Map';
 import ScheduleScreen from '../screens/Schedule';
@@ -18,10 +19,44 @@ const AboutStack = createStackNavigator({
     About: AboutScreen
 });
 
-const TabNavigator = createBottomTabNavigator({
-    Schedule: ScheduleStack,
-    Map: MapStack,
-    Faves: FavesStack,
-    About: AboutStack,
-});
+const TabNavigator = createBottomTabNavigator(
+    {
+        Schedule: ScheduleStack,
+        Map: MapStack,
+        Faves: FavesStack,
+        About: AboutStack,
+    },
+    {
+        // defaultNavigationOptions: ({ navigation }) => ({
+        //     tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        //         const { routeName } = navigation.state;
+        //         //     let IconComponent = Ionicons;
+        //         //     let iconName;
+        //         //     if (routeName === 'Home') {
+        //         //         iconName = focused
+        //         //             ? 'ios-information-circle'
+        //         //             : 'ios-information-circle-outline';
+        //         //         // Sometimes we want to add badges to some icons.
+        //         //         // You can check the implementation below.
+        //         //         IconComponent = HomeIconWithBadge;
+        //         //     } else if (routeName === 'Settings') {
+        //         //         iconName = focused ? 'ios-list-box' : 'ios-list';
+        //         //     }
+
+        //         //     // You can return any component that you like here!
+        //         //     return <IconComponent name={iconName} size={25} color={tintColor} />;
+        //     },
+        // }),
+        tabBarOptions: {
+            activeTintColor: 'white',
+            inactiveTintColor: 'grey',
+            labelStyle: {
+                fontSize: 14
+            },
+            style: {
+                backgroundColor: "black"
+            }
+        }
+    }
+);
 export default TabNavigator;
