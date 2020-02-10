80e623bee022e1f91fed26fdd352f288f0ec5d95
Fiona <fyeung21@gmail.com>
2020-02-10 02:33:03 -0800

    display faves on fave screen

--- js/screens/Faves/Faves.js
+++ js/screens/Faves/Faves.js
@@ -1,18 +1,56 @@
-import React from "react";
-import { View, ScrollView, Text } from "react-native";
-import { AsyncStorage } from "../../components/CodeofConduct/AsyncTest";
+import React, { useContext } from 'react'
+import { View, Text, SectionList } from 'react-native'
+import { withNavigation } from 'react-navigation'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+import { FavesContext } from '../../context/FavesContext/FavesContext'
+import { formatSessionData } from '../../components/helpers/dataFormatHelper'
+import SessionCard from '../../components/SessionCard/SessionCard'
+import globalStyles from '../../globalStyles'
+
+const GET_SESSIONS = gql`
+    query {
+        allSessions {
+            id
+            startTime
+            title
+            location
+        }
+    }
+`;
 
 const Faves = () => {
+    const { loading, error, data } = useQuery(GET_SESSIONS)
+
+    const { faveIds } = useContext(FavesContext)
+
+    if (loading) return <Text>Loading</Text>;
+    if (error) return <Text>Error</Text>;
+
+    const filterFaves = data.allSessions.filter(data => faveIds.includes(data.id))
+
+    const groupedSessions = formatSessionData(filterFaves)
+
     return (
-        <ScrollView>
-            <View>
-                <Text>
-                    Faves
-                </Text>
-                {/* <AsyncStorage /> */}
-            </View>
-        </ScrollView>
+        <View>
+            <SectionList
+                sections={groupedSessions}
+                keyExtractor={(item, index) => item + index}
+                renderItem={({ item }) =>
+                    <SessionCard
+                        id={item.id}
+                        title={item.title}
+                        location={item.location}
+                    >
+                    </SessionCard>}
+                renderSectionHeader={({ section: { title } }) => (
+                    <View style={globalStyles.sectionTitleCont}>
+                        <Text style={globalStyles.sectionTitle}>{title}</Text>
+                    </View>
+                )}
+            />
+        </View>
     )
 }
 
-export default Faves;
+export default withNavigation(Faves)
