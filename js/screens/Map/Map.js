4a7c75666ff327048bb43733d217e77afb0919ed
Fiona <fyeung21@gmail.com>
2020-03-05 12:46:18 -0800

    add placeholder map

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { View, ScrollView, Text, Image } from "react-native";
+import { View, ScrollView, Image, StyleSheet } from "react-native";
 
 const Map = () => {
     return (
         <ScrollView>
             <View>
-                <Text>
-                    Map
-                </Text>
-                <Image source={require("../../assets/images/map_pin.png")} />
+                {/* <Image source={require("../../assets/images/map_pin.png")} /> */}
+                <Image style={styles.map} source={require("../../assets/images/map.png")} />
             </View>
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    map: {
+        height: 700
+    },
+})
 
 export default Map;
